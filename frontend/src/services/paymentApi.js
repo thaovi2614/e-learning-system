138cc152f877d4f29b3572ac126ff8c526d74390
yuantran <uyentran12348@gmail.com>
2026-05-10 13:19:15 +0700

    fix payment error

--- frontend/src/services/paymentApi.js
+++ frontend/src/services/paymentApi.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export const checkoutCart = () => {
-    return api.post("/payments/momo/create");
+    return api.post("/payments/momo/create", {});
 };
 
 export const createMomoPayment = (courseIds = []) => {
